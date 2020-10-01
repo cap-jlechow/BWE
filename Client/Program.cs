69c61d2f4322de98a4416019b40a37609532466d
John Lechowicz <jlechow@cap.org>
2020-10-01 16:51:29 -0500

    Added logging example

--- Client/Program.cs
+++ Client/Program.cs
@@ -17,6 +17,8 @@ namespace BlazorWasmElectron.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            builder.Logging.SetMinimumLevel(LogLevel.Debug);
+            
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
